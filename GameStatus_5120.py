79efabfad18b27d0baa448171975912f3b1b8958
DiegoMontesKid <154762565+DiegoMontesKid@users.noreply.github.com>
2025-10-18 21:30:27 -0700

    innit

---
+++ GameStatus_5120.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+
+
+class GameStatus:
+
+
+	def __init__(self, board_state, turn_O):
+
+		self.board_state = board_state
+		self.turn_O = turn_O
+		self.oldScores = 0
+
+		self.winner = ""
+
+
+	def is_terminal(self):
+		for i in self.board_state:
+			for j in i:
+				if j == 0:
+					return False
+		return True
+		"""
+        YOUR CODE HERE TO CHECK IF ANY CELL IS EMPTY WITH THE VALUE 0. IF THERE IS NO EMPTY
+        THEN YOU SHOULD ALSO RETURN THE WINNER OF THE GAME BY CHECKING THE SCORES FOR EACH PLAYER 
+        """
+		
+
+	def get_scores(self, terminal):
+		"""
+        YOUR CODE HERE TO CALCULATE THE SCORES. MAKE SURE YOU ADD THE SCORE FOR EACH PLAYER BY CHECKING 
+        EACH TRIPLET IN THE BOARD IN EACH DIRECTION (HORIZONAL, VERTICAL, AND ANY DIAGONAL DIRECTION)
+        
+        YOU SHOULD THEN RETURN THE CALCULATED SCORE WHICH CAN BE POSITIVE (HUMAN PLAYER WINS),
+        NEGATIVE (AI PLAYER WINS), OR 0 (DRAW)
+        
+        """        
+		rows = len(self.board_state)
+		cols = len(self.board_state[0])
+		scores = 0
+		check_point = 3 if terminal else 2
+
+		for  s in self.board_state:
+			for  index in range(rows):
+				if index + 3 > rows:
+					break
+				if s[index] == 1 and s[index + 1] == 1 and s[index + 2] == 1:
+					scores += 1
+				if s[index] == 2 and s[index + 1] == 2 and s[index + 2] == 2:
+					scores -= 1
+		
+		for i in range(rows):
+			if i + 3 > rows:
+					break
+			for j in range(rows):
+				if self.board_state[i][j] == 1 and self.board_state[i + 1][j] == 1 and self.board_state[i + 2][j] == 1:
+					scores += 1
+				if self.board_state[i][j] == 2 and self.board_state[i + 1][j] == 2 and self.board_state[i + 2][j] == 2:
+					scores += 1
+
+		cross_times = rows - 2
+
+		for c in range(cross_times):
+			if self.board_state[c][c] == 1 and self.board_state[c + 1][c + 1] == 1 and self.board_state[c + 2][c + 2] == 1:
+				scores += 1
+			if self.board_state[c][c] == 2 and self.board_state[c + 1][c + 1] == 2 and self.board_state[c + 2][c + 2] == 2:
+				scores -= 1
+		
+		
+	    
+
+	def get_negamax_scores(self, terminal):
+		"""
+        YOUR CODE HERE TO CALCULATE NEGAMAX SCORES. THIS FUNCTION SHOULD EXACTLY BE THE SAME OF GET_SCORES UNLESS
+        YOU SET THE SCORE FOR NEGAMX TO A VALUE THAT IS NOT AN INCREMENT OF 1 (E.G., YOU CAN DO SCORES = SCORES + 100 
+                                                                               FOR HUMAN PLAYER INSTEAD OF 
+                                                                               SCORES = SCORES + 1)
+        """
+		rows = len(self.board_state)
+		cols = len(self.board_state[0])
+		scores = 0
+		check_point = 3 if terminal else 2
+	    
+
+	def get_moves(self):
+		moves = []
+		"""
+        YOUR CODE HERE TO ADD ALL THE NON EMPTY CELLS TO MOVES VARIABLES AND RETURN IT TO BE USE BY YOUR
+        MINIMAX OR NEGAMAX FUNCTIONS
+        """
+		return moves
+
+
+	def get_new_state(self, move):
+		new_board_state = self.board_state.copy()
+		x, y = move[0], move[1]
+		new_board_state[x,y] = 1 if self.turn_O else -1
+		return GameStatus(new_board_state, not self.turn_O)
